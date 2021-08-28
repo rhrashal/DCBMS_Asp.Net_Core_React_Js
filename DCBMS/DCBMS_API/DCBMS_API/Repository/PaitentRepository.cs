1b8b912794fbbf205aafa432e4dba81021068490
1251184-Robiul Hossain <rhrashal50@gmail.com>
2021-08-28 21:59:43 +0600

    add number to bangla text function

--- DCBMS/DCBMS_API/DCBMS_API/Repository/PaitentRepository.cs
+++ DCBMS/DCBMS_API/DCBMS_API/Repository/PaitentRepository.cs
@@ -155,6 +155,7 @@ namespace DCBMS_API.Repository
                     response.Add(rpt);
                 }
                 response[0].GrandTotal = grandTotal;
+                var date = NumberToWords(Convert.ToInt32( grandTotal));
 
                 return response;
             }
@@ -166,7 +167,59 @@ namespace DCBMS_API.Repository
 
             
         }
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "শূন্য";
 
+            if (number < 0)
+                return "মাইনাস " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+            if ((number / 10000000) > 0)
+            {
+                words += NumberToWords(number / 10000000) + " কোটি ";
+                number %= 10000000;
+            }
+            if ((number / 100000) > 0)
+            {
+                words += NumberToWords(number / 100000) + " লাখ ";
+                number %= 100000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " হাজার ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " শত ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += " ";
+                //words += "এবং ";
+
+                var unitsMap = new[] { "শূন্য", "এক", "দুই", "তিন", "চার", "পাঁচ", "ছয়", "সাত", "আট", "নয়", "দশ", "এগারো", "বারো", "তেরো", "চৌদ্দ", "পনেরো", "ষোল", "সতেরো", "আঠারো", "ঊনিশ", "বিশ", "একুশ", "বাইশ", "তেইশ ", "চব্বিশ", "পঁচিশ", "ছাব্বিশ", "সাতাশ", "আঠাশ", "ঊনত্রিশ", "ত্রিশ", "একত্রিশ", "বত্রিশ", "তেত্রিশ", "চৌত্রিশ", "পঁয়ত্রিশ", "ছত্রিশ", "সাঁইত্রিশ", "আটত্রিশ", "ঊনচল্লিশ", "চল্লিশ", "একচল্লিশ", "বিয়াল্লিশ", "তেতাল্লিশ", "চুয়াল্লিশ", "পঁয়তাল্লিশ", "ছেচল্লিশ", "সাতচল্লিশ", "আটচল্লিশ", "ঊনপঞ্চাশ", "পঞ্চাশ", "একান্ন", "বায়ান্ন", "তিপ্পান্ন", "চুয়ান্ন", "পঞ্চান্ন", "ছাপ্পান্ন", "সাতান্ন", "আটান্ন", "ঊনষাট", "ষাট", "একষট্টি", "বাষট্টি", "তেষট্টি", "চৌষট্টি", "পঁয়ষট্টি", "ছেষট্টি", "সাতষট্টি", "আটষট্টি", "ঊনসত্তর", "সত্তর", "একাত্তর", "বাহাত্তর", "তিয়াত্তর", "চুয়াত্তর", "পঁচাত্তর", "ছিয়াত্তর", "সাতাত্তর", "আটাত্তর", "ঊনআশি", "আশি", "একাশি", "বিরাশি", "তিরাশি", "চুরাশি", "পঁচাশি", "ছিয়াশি", "সাতাশি", "আটাশি", "ঊননব্বই", "নব্বই", "একানব্বই", "বিরানব্বই", "তিরানব্বই", "চুরানব্বই", "পঁচানব্বই", "ছিয়ানব্বই", "সাতানব্বই", "আটানব্বই", "নিরানব্বই", "একশো" };
+                var tensMap = new[] { "শূন্য", "দশ", "বিশ", "ত্রিশ", "চল্লিশ", "পঞ্চাশ", "ষাট", "সত্তর", "আশি", "নব্বই" };
+
+                if (number < 100)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
         public async Task<List<TypeWiseReportVM>> TypeWiseReport(FilterVM filter)
         {
             try
